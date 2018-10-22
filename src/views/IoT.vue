e4227d20538ee8e211703c819e7b2ea0a2ae644e
Bryan Jim Paano <bryanjim.paano@gmail.com>
2018-10-22 21:20:55 +0800

    IoT code update

--- src/views/IoT.vue
+++ src/views/IoT.vue
@@ -1,9 +1,24 @@
 <template>
  <v-container>
-    <v-btn square color="black" outline @click="livingRoomSwitch(isLivingRoom)">Living Room Switch</v-btn>
+    <v-btn square color="black" outline @click="livingRoomSwitch()">Living Room Switch</v-btn>
     <div>{{isLivingRoom}}</div>
     <v-btn square color="black" outline @click="fetchDatabase()">Fetch Database</v-btn>
-    <div>{{timeStamps}}</div>
+    <table id="firstTable" v-if="database.length > 0">
+      <thead>
+        <tr>
+          <th>Time Stamp</th>
+          <th>PIR Module</th>
+          <th>Living Room</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(data, index) in database" :key="index">
+          <td>{{data.timeStamp}}</td>
+          <td>{{data.pirModule}}</td>
+          <td>{{data.livingRoomSwitch}}</td>
+        </tr>
+      </tbody>
+    </table>
  </v-container>
 
 </template>
@@ -14,11 +29,16 @@ export default {
   data() {
     return {
       isLivingRoom: false,
-      timeStamps: {}
+      database: [],
+      databaseModel: {
+        timeStamp: "",
+        pirModule: 0,
+        livingRoomSwitch: 0
+      }
     };
   },
   methods: {
-    livingRoomSwitch(mode) {
+    livingRoomSwitch() {
       this.isLivingRoom = !this.isLivingRoom;
       if (this.isLivingRoom) {
         axios.get("http://192.168.43.202:8080/livingRoomSwitch/On");
@@ -27,9 +47,48 @@ export default {
       }
     },
     async fetchDatabase() {
-        this.timeStamps = (await axios.get("http://192.168.43.202:8080/fetchDatabase")).data
-        console.log(this.timeStamps)
+      this.database = [];
+      let response = (await axios.get(
+        "http://192.168.43.202:8080/fetchDatabase"
+      )).data;
+      response.forEach(data => {
+        this.databaseModel.timeStamp = data[0];
+        this.databaseModel.pirModule = data[1];
+        this.databaseModel.livingRoomSwitch = data[2];
+        this.database.push(this.databaseModel);
+        this.databaseModel = []
+      });
     }
   }
 };
 </script>
+<style scoped>
+table {
+  font-family: "Open Sans", sans-serif;
+  width: 750px;
+  border-collapse: collapse;
+  border: 3px solid #44475c;
+  margin: 10px 10px 0 10px;
+}
+
+table th {
+  text-transform: uppercase;
+  text-align: left;
+  background: #44475c;
+  color: #fff;
+  padding: 8px;
+  min-width: 30px;
+}
+
+table td {
+  text-align: left;
+  padding: 8px;
+  border-right: 2px solid #7d82a8;
+}
+table td:last-child {
+  border-right: none;
+}
+table tbody tr:nth-child(2n) td {
+  background: #d4d8f9;
+}
+</style>
